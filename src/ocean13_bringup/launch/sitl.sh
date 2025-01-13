6c382cfa538f0b1ab039194afca45482d7caebc4
robo2020 <robo2020@gmail.com>
2025-01-13 05:59:33 +0200

    docker builds

--- src/ocean13_bringup/launch/sitl.sh
+++ src/ocean13_bringup/launch/sitl.sh
@@ -5,4 +5,4 @@
 --speedup 1 --defaults /workspaces/ocean13_ws/src/ocean13_bringup/config/boat.params  -I0
 
 
-sim_vehicle.py -v APMrover2 -f JSON --add-param-file=/home/user/workspaces/ocean13_ws/src/ocean13_bringup/config/boat.params  --console 
+# sim_vehicle.py -v APMrover2 -f JSON --add-param-file=/home/user/workspaces/ocean13_ws/src/ocean13_bringup/config/boat.params  --console 
